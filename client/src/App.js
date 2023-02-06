fcc114ff71e4f11e9f471d878d1e2608eecfbd84
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-02-05 21:59:49 -0500

    Updated state mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
